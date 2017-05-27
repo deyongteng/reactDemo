abc5db0c49d3e0025aec81d28b31c81714bb8dac
deyongteng <906447370@qq.com>
2017-05-27 22:41:17 +0800

    test

--- js/index.js
+++ js/index.js
@@ -2,8 +2,3 @@
  * Created by Administrator on 2017/5/27.
  */
 var box = document.getElementById("box")
-box.style.width="100px";
-box.style.height="100px";
-box.style.background="red";
-box.style.marginLeft="100px";
-box.style.transition="1s";
