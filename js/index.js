94c9d9c5dd3cb69e2c45ced9192a7da088da3621
deyongteng <906447370@qq.com>
2017-05-27 12:33:18 +0800

    test

--- js/index.js
+++ js/index.js
@@ -2,4 +2,7 @@
  * Created by Administrator on 2017/5/27.
  */
 var box = document.getElementById("box")
+box.style.width="100px";
+box.style.height="100px";
+box.style.background="red";
 console.log(box)
